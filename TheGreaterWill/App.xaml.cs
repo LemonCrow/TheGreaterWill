4c89c4123b7e5b64f887393ba6f45597de9221af
이준서 <lemon_crow@naver.com>
2023-12-07 15:26:24 +0900

    로고 스플래시 xaml 완성 3초간 화면에 떠있음 추후 로고 이미지 변경 요망

--- TheGreaterWill/App.xaml.cs
+++ TheGreaterWill/App.xaml.cs
@@ -13,5 +13,23 @@ namespace TheGreaterWill
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            // Logo.xaml 윈도우 생성
+            var splashScreen = new TheGreaterWill.Logo.Logo();
+            splashScreen.Show();
+
+            // 3초 대기
+            System.Threading.Thread.Sleep(3000);
+
+            // Logo.xaml 닫기
+            splashScreen.Close();
+
+            // MainWindow 표시
+            var mainWindow = new MainWindow();
+            mainWindow.Show();
+        }
     }
 }
