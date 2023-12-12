4e181c498718f9d5ae90c779b1e18cef807dc891
이준서 <lemon_crow@naver.com>
2023-12-12 10:54:47 +0900

    캐릭터 데이터 가져와서 압축파일 만들기 구현 완료 -캐릭터 이름으로 폴더생성(있으면 안생성) -세이브 제목 + 년-월-일-시-분-초 형식으로 압축 파일 생성 및 저장 -만약 세이브제목이 없을 시 경고 메시지 출력 및 실행 안함

---
+++ TheGreaterWill/Models/SaveManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheGreaterWill.ViewModels.Property;
+
+namespace TheGreaterWill.Models
+{
+    internal class SaveManager
+    {
+        internal bool FileSave(List<SaveInfoData> ch, string saveName)
+        {
+            try
+            {
+                if (!Directory.Exists(ch[0].ChName))
+                {
+                    Directory.CreateDirectory(ch[0].ChName);
+                }
+
+                DateTime now = DateTime.Now;
+                string formattedData = now.ToString("yyyy-MM-dd-HH-mm-ss");
+
+                saveName += " " + formattedData;
+
+                string zipFilePath = Path.Combine(ch[0].ChName, saveName + ".zip");
+
+                ZipFile.CreateFromDirectory(ch[0].ChPath, zipFilePath);
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
--- TheGreaterWill/ViewModels/MainWindowViewModel.cs
+++ TheGreaterWill/ViewModels/MainWindowViewModel.cs
@@ -6,6 +6,8 @@ using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using CommunityToolkit.Mvvm.Input;
 using TheGreaterWill.Models;
 using TheGreaterWill.ViewModels.Property;
 
@@ -14,7 +16,13 @@ namespace TheGreaterWill.ViewModels
     internal class MainWindowViewModel : INotifyPropertyChanged
     {
         private JsonEditor jsonEditor;
+        private SaveManager saveManager;
         private List<SaveInfoData> saveInfos;
+
+        private string _saveName;
+        private string _alertMsg;
+
+        public ICommand SaveCommand { get; private set; }
 
         public List<SaveInfoData> SaveData
         {
@@ -26,10 +34,43 @@ namespace TheGreaterWill.ViewModels
             }
         }
 
+        public string SaveName
+        {
+            get { return _saveName; }
+            set
+            {
+                _saveName = value;
+                OnPropertyChanged(nameof(SaveName));
+            }
+        }
+
+        public string AlertMsg
+        {
+            get { return _alertMsg; }
+            set
+            {
+                _alertMsg = value;
+                OnPropertyChanged(nameof(AlertMsg));
+            }
+        }
+
         public MainWindowViewModel()
         {
             jsonEditor = new JsonEditor();
+            saveManager = new SaveManager();
             SaveData = jsonEditor.LoadJsonFile();
+            SaveCommand = new RelayCommand(SaveCommandWrapper);
+        }
+
+        private void SaveCommandWrapper()
+        {
+            if (SaveName != null && SaveName != "")
+            {
+                saveManager.FileSave(SaveData, SaveName);
+                AlertMsg = "";
+            }
+            else
+                AlertMsg = "제목을 작성해주세요.";
         }
 
         public event PropertyChangedEventHandler? PropertyChanged;
